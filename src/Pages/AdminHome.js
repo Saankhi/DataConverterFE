5457c9b03b42448983e71e4703aed7079c5978a8
Saankhi <sankhi9katari27@gmail.com>
2023-06-21 09:38:31 +0530

    click-here-navigation

--- src/Pages/AdminHome.js
+++ src/Pages/AdminHome.js
@@ -1,16 +1,18 @@
 import React from "react"
-import Header from "../Components/Header"
+import { useNavigate } from "react-router-dom"
 
 
 export default function AdminHome() {
+
+    const navigate = useNavigate();
+
     return (
         <>
-            {/* <Header /> */}
             <div style={{ textAlign: 'center', marginTop: '200px' }}>
                 <h1>WELCOME TO OUR <b style={{ color: "#12B5B0" }} >WEBSITE</b></h1>
                 <h4>DATA CONVERTER</h4>
                 <p>Use this website to convert your file</p>
-                <button style={{ width: '450px', backgroundColor: '#12B5B0', height: '50px', borderRadius: '5px', border:"none", color: '#fff' }}>Click here</button>
+                <button onClick={navigate('/template')} style={{ width: '450px', backgroundColor: '#12B5B0', height: '50px', borderRadius: '5px', border: "none", color: '#fff' }}>Click here</button>
             </div>
         </>
     )
